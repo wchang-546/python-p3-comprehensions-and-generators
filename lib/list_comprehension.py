0cd1be088232e0d6d31a83c1c2b607669f3e5b54
Will Chang <william.chaang@gmail.com>
2023-07-25 17:09:32 -0600

    List comprehension practiced. This was good

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [i for i in num_list if i % 2 == 0]
+
+# Non-list comprehension method
+# new_evens = []
+# for i in num_list: 
+#    if i % 2 == 0:
+#        new_evens.append(i)
+#    else: 
+#        None
+# return new_evens
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [i+"!" for i in sentence_list]
+
+#Non-list comprehension method, wasn't really working.
+    # new_sentence_list = []
+    # if sentence_list:
+    #     for i in sentence_list: 
+    #         new_sentence_list.append(i+"!")
+    #     return new_sentence_list
+    # else: 
+    #     return new_sentence_list
